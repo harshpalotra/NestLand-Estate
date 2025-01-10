054b6f5d64bc8aa2ddc499d52753a3bbe5fe0f0f
harshpalotra <palotraharsh@gmail.com>
2025-01-11 01:32:46 +0530

    complete delete listing functionality

--- Server/Controllers/user.controller.js
+++ Server/Controllers/user.controller.js
@@ -3,75 +3,98 @@ import bcryptjs from 'bcryptjs';
 import User from '../Models/user.models.js';
 import Listing from "../Models/listing.models.js";
 
-export const test = (req,res)=>{
+export const test = (req, res) => {
     res.json({
-        message:'Hello World',
+        message: 'Hello World',
     });
 };
 
 
-export const updateUser = async (req,res, next) =>{
-   
-    if(req.user?.id !== req.params.id) return next(errorHandler(401,"You can only update your own account"));
+export const updateUser = async (req, res, next) => {
+
+    if (req.user?.id !== req.params.id) return next(errorHandler(401, "You can only update your own account"));
     try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password,10);
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
 
-        const updateUser = await User.findByIdAndUpdate(req.params.id,{
-            $set:{
-                username:req.body.username,
-                email:req.body.email,
-                password:req.body.password,
-                avatar:req.body.avatar,
-            },                                             
-        },{
-            new:true
+        const updateUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            },
+        }, {
+            new: true
         });
-        
-        const{password, ...rest} = updateUser._doc;
+
+        const { password, ...rest } = updateUser._doc;
         res.status(200).json(rest);
     } catch (error) {
         next(error);
     }
 };
 
-export const deleteUser = async (req,res,next) =>{
-    if(req.user.id !== req.params.id) return next(errorHandler(401,"You can only delete your own account"));
-    try{
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your own account"));
+    try {
         await User.findByIdAndDelete(req.params.id)
         res.clearCookie('access_token');
         res.status(200).json("Account has been deleted");
 
-        
-    } catch (error){
+
+    } catch (error) {
         next(error);
     }
 };
 
-export const signOut = async (req,res,next) =>{
+export const signOut = async (req, res, next) => {
 
     try {
         res.clearCookie('access_token');
         res.status(200).json("Signout successfully");
-        
+
     } catch (error) {
         next(error);
     }
 
 }
 
-export const getUserListing = async (req,res,next) =>{
-    if(req.user.id === req.params.id) {
+export const getUserListing = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
         try {
-          const listing = await Listing.find({ userRef: req.params.id});
+            const listing = await Listing.find({ userRef: req.params.id });
             res.status(200).json(listing);
         }
         catch (error) {
             next(error);
-}
-}
-else {
-    return next(errorHandler(401,"You can only view your own account"));
-}
+        }
+
+    }
+    else {
+        return next(errorHandler(401, "You can only view your own account"));
+    }
+};
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"))
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "you can only delete your own listing"));
+
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted');
+
+    }
+    catch (error) {
+        next(error);
+    }
+
 };
--- Server/Routes/listing.router.js
+++ Server/Routes/listing.router.js
@@ -1,8 +1,10 @@
 import express, { Router } from 'express';
 import { createListing } from '../Controllers/listing.controller.js';
 import { verfityToken } from '../utils/verify.User.js';
+import { deleteListing } from '../Controllers/user.controller.js';
 const listingRouter = express.Router();
 
 listingRouter.post('/create', verfityToken , createListing);
+listingRouter.delete('/delete/:id', verfityToken, deleteListing)
 
 export default listingRouter;
