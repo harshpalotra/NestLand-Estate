0910aa735c48924cd8c3247e5e733097d3a7eb5c
harshpalotra <128576838+harshpalotra@users.noreply.github.com>
2025-02-06 17:22:15 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { persistor, store } from './Redux/store.js';
-import { Provider } from 'react-redux'; // Ensure this is included
+import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 
+// Loader component for rehydration
+const Loader = () => <div className="loader">Loading...</div>;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}> {/* Wrap App with Provider */}
-      <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
         <App />
-      </PersistGate>  
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
