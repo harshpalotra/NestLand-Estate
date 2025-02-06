252fae98d2f3e72e7ef6e43e12686c82b14961d6
harshpalotra <128576838+harshpalotra@users.noreply.github.com>
2025-02-06 18:02:09 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,55 +1,62 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-
 import userRouter from './Routes/user.route.js';
 import authRouter from './Routes/auth.route.js';
+import cookieParser from "cookie-parser";
 import listingRouter from './Routes/listing.router.js';
-
+import path from 'path';
+import inherits from 'inherits';
+global.inherits = inherits;
+import cors from 'cors';
+import helmet from 'helmet';
 dotenv.config();
 
-// Connect to MongoDB
+
+// Connect to MongoDB using the environment variable
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err.message));
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.log('Error connecting to MongoDB:', err.message);
+});
 
 const __dirname = path.resolve();
+
 const app = express();
-
-// Middleware
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors({ origin: 'https://nestland-estate.onrender.com', credentials: true }));
 
-// Routes
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
 app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use('/api/auth', authRouter)
+app.use('/api/listing',listingRouter)
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+})
 
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = process.env.NODE_ENV === 'production' ? 'Internal Server Error' : err.message;
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+// To manage error we making err middleware 
+app.use((err, req, res, next) =>{
+const statusCode = err.statusCode || 500;
+const message = err.message || 'Internal server Error';
+return res.status(statusCode).json({
+  success: false,
+  statusCode,
+  message
 });
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
